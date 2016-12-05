ca0831143e570e2465b0898220dc7f7916d50894
Nghia Nguyen <nghiaround@nghiaround-macbookpro.roam.corp.google.com>
2016-12-04 17:06:19 -0800

    First Draft

---
+++ Source/Section03_01/Section03_01.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Section03_01.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Section03_01, "Section03_01" );
---
+++ Source/Section03_01/Section03_01GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Section03_01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION03_01_API ASection03_01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
